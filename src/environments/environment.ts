b85cbea6d9277104dc65796c6cf0a5e07a06c88c
Dan Kimchi <dan.k@mishol.org>
2019-07-03 11:31:54 +0300

    פתיחה של test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBUgm-OHjg-cpOXyZxTD6KMANyxHLgQtfs',
-    authDomain: 'travelregistration-b5dbd.firebaseapp.com',
-    databaseURL: 'https://travelregistration-b5dbd.firebaseio.com',
-    projectId: 'travelregistration-b5dbd',
-    storageBucket: 'travelregistration-b5dbd.appspot.com',
-    messagingSenderId: '79605643022',
-    appId: '1:79605643022:web:0d09c345d121d6a7'
+    apiKey: 'AIzaSyDALtWg2SViSymCF9-uf66COx1r_PoGDVo',
+    authDomain: 'testtravelregistration-130783.firebaseapp.com',
+    databaseURL: 'https://testtravelregistration-130783.firebaseio.com',
+    projectId: 'testtravelregistration-130783',
+    storageBucket: 'testtravelregistration-130783.appspot.com',
+    messagingSenderId: '903064996507',
+    appId: '1:903064996507:web:754de885ec927090'
   }
 };
 /*
